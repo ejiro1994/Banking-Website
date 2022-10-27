616f4d78a5dd1d3408797d588217231e3bf02a34
ejiro1994 <ejiro1994@gmail.com>
2022-10-27 23:46:15 +0100

    assets folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,7 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
-  );
+const App = () => {
+  return <div>hello</div>
 }
 
-export default App;
+export default App
